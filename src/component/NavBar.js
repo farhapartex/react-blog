ac85091f9f54a9b3296eb84cb11088a3b30d9507
Md Nazmul Hasan <farhapartex@gmail.com>
2021-04-29 13:21:35 +0600

    image added in blog detail page

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -4,22 +4,22 @@ import { Link } from "react-router-dom"
 class NavBar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Hasan's Blog</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Hasan's Blog</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
                             <Link to="/" className="nav-link">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About Me</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About Me</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
                         </li>
                     </ul>
                 </div>
--- src/component/SingleBlogBox.js
+++ src/component/SingleBlogBox.js
@@ -5,12 +5,12 @@ class SingleBlogBox extends Component {
     render() {
         return (
             <div className="w-100 single-blog-box pt-3 pb-3">
-                <h3><Link to="/detail">India COVID death toll tops 200,000 as essential supplies run out</Link></h3>
-
-                <div>
+                <h3><Link to="/detail"><b>India COVID death toll tops 200,000 as essential supplies run out</b></Link></h3>
+                <div className="w-100"><span>February 2021</span> <span className="ml-2"><b>AWS</b></span></div>
+                <div className="w-100 mt-2">
                     <p>India’s death toll from COVID-19 has crossed the grim milestone of 200,000 as the country continued to face shortages of hospital beds, medical oxygen and other essential supplies.</p>
                 </div>
-                <a href="#">Details</a>
+                <Link to="/detail">Read</Link>
             </div>
         )
     }
